c69b6972623f1bd77b0705da7379bac0498435c6
Sabrina Barr <sabrinabarr@Sabrinas-MacBook-Pro.local>
2017-11-24 19:45:21 -0500

    added result div

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,44 +2,55 @@ $(document).ready(function(){
 
 
 var game = {
-	wins: 	"",
-	loses:  "",
-	numCount: "",
-	userScore: "",
-	garnetRandNum: "",
-	rubyRandNum: "",
-	diamondRandNum: "",
-	saphireRandNumb: "",
-	gameRandomNumber: "",
+	wins: 	0,
+	losses:  0,
+	numCount: 0,
+	userScore: 0,
+	garnetRandNum: 0,
+	rubyRandNum: 0,
+	diamondRandNum: 0,
+	saphireRandNumb: 0,
+	gameRandomNumber: 0,
 
 	startGame: function() {
 		wins = 0;
-		loses = 0;
-		numCount = 39;
+		losses = 0;
+		numCount = 0;
 		userScore = 0;
 		// append the above vars to their various divs
-		// init game here   
-		gameRandmomNumber= getRandomNumber();
-	    saphiRandNum = getCrystalRandomNumber();
-	    rubyRandNum = getCrystalRandomNumber();
-	    diamondRandNum = getCrystalRandomNumber();
-		garnetRandNumb = getRandomNumber();
+		$('#totalScore').text("Total Score: " + userScore);
+		$('#game-message').text("");
+		gameRandmomNumber = this.getRandomNumber();
+		$('#random-number').text('Match this number to win: ' + gameRandmomNumber );
+
+		// These are hidden
+	    saphireRandNum = this.getCrystalRandomNumber();
+	    rubyRandNum = this.getCrystalRandomNumber();
+	    diamondRandNum = this.getCrystalRandomNumber();
+		garnetRandNum = this.getCrystalRandomNumber();
+		console.log(gameRandmomNumber);
+		console.log(saphireRandNum);
+		console.log(rubyRandNum);
+		console.log(garnetRandNum);
+		console.log(diamondRandNum);
 	} ,
 
 	getRandomNumber: function() {
 		// add code to get game generated random number here
-	  randomNumber = Math.floor(Math.random() * 120) + 19 ;
+	  randomNumber = Math.floor(Math.random() * 121) + 19 ;
 	  return randomNumber;
 	},
 		
 	getCrystalRandomNumber: function() {
 		//   add code to genere
-		var randomNumber = Math.floor(Math.random() * 120) + 19 ;
+		var randomNumber = Math.floor(Math.random() * 13) + 1 ;
 		return randomNumber;
 	},
 
 	updateUserScore: function(number) {
 		this.userScore += number;
+
+
 		// update user score here
 	},
 
@@ -50,8 +61,58 @@ var game = {
 
 	},
 
+	gameResult: function(result) {
+		$('.main-body').hide();
+        $('.result-body').show();
+        $('#game-message').text(result);
+
+        $('#result-button').on("click", function() {
+        		$('.main-body').show();
+        		$('.result-body').hide();
+        		game.startGame();
+        });
+
+
+	}
+
 };
    $('.gems').on("click", function(e) {
-   		console.log(this);
+   		console.log(this.id);
+
+   		switch (this.id ) {
+
+			case "diamond": 
+				console.log("you just clicked " + this.id);
+				userScore += diamondRandNum;
+				break;
+			case "ruby": 
+				console.log("you just clicked ruby" + this.id);
+				 userScore += rubyRandNum;
+				break;
+			case "saphire": 
+				console.log("you just clicked " + this.id);
+				userScore += saphireRandNum;
+				break;
+			case "garnet": 
+				console.log("you just clicked " + this.id);
+				userScore += garnetRandNum;
+				break;			
+		}
+         
+		$('#totalScore').text("Total Score: " + userScore);
+
+		if (userScore === gameRandmomNumber) {
+			game.wins++;
+			$('#wins').text('Wins: ' + wins);
+			     var result = "You win!!!"				
+				gameResult(result);			
+		} else if ( userScore > gameRandmomNumber) {
+				game.losses++;
+				$('#losses').text('Losses: ' + losses);
+				var result = "You lose!!!"
+				game.gameResult(result);				
+			
+		}
    });
+   game.startGame();
 });
